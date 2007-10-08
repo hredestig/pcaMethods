303168d85180d0535a4c58a753dcfd9f0adc8e99
w.stacklies <w.stacklies@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-10-08 10:33:45 +0000

    Fixed tiny bug in the slplot function preventing the ellipse to be drawn
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pcaMethods@27836 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pca.R
+++ R/pca.R
@@ -283,7 +283,7 @@ fitted.pcaRes <- function(object, data=NULL, nPcs=object@nPcs, ...) {
                data <- sweep(data, 2, object@center)
              scores <- data %*% object@loadings[,1:nPcs, drop=FALSE]
            }
-           else
+           if(is.null(data))
              scores <- object@scores[,1:nPcs, drop=FALSE]
            recData <- tcrossprod(scores, object@loadings[,1:nPcs, drop=FALSE])
            if(object@centered)
@@ -621,7 +621,7 @@ simpleEllipse <- function(x, y, alfa=0.95, len=200) {
 
   N <- length(x)
   A <- 2
-  mypi <- seq(0, 2 * pi, len)
+  mypi <- seq(0, 2 * pi, length=len)
   r1 <- sqrt(var(x) * qf(alfa, 2, N - 2) * (2*(N^2 - 1)/(N * (N - 2))))
   r2 <- sqrt(var(y) * qf(alfa, 2, N - 2) * (2*(N^2 - 1)/(N * (N - 2))))
   cbind(r1 * cos(mypi) + mean(x), r2 * sin(mypi) + mean(y))
