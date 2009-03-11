61ea818026766c9a64732a326ce262d01a1f0551
h.redestig <h.redestig@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-03-11 11:09:32 +0000

    added kevin's biplot function and some getter functions
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pcaMethods@37906 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/generic.R
+++ R/generic.R
@@ -1,8 +1,20 @@
 setGeneric("slplot", function(object, pcs=c(1,2), scoresLoadings=c(TRUE, TRUE),
                               sl="def", ll="def",
-                              hotelling=0.95, rug=TRUE, sub=NULL,...) standardGeneric("slplot"))
+                              hotelling=0.95, rug=TRUE, sub=NULL,...)
+           standardGeneric("slplot"))
 
 setGeneric("vector2matrices", function(object, ...) standardGeneric("vector2matrices"))
+setGeneric("leverage", function(object, ...) standardGeneric("leverage"))
+
+## getters
+setGeneric("nPcs", function(object, ...) standardGeneric("nPcs"))
+setGeneric("nObs", function(object, ...) standardGeneric("nObs"))
+setGeneric("nVar", function(object, ...) standardGeneric("nVar"))
+setGeneric("completeObs", function(object, ...) standardGeneric("completeObs"))
+setGeneric("centered", function(object, ...) standardGeneric("centered"))
+setGeneric("method", function(object, ...) standardGeneric("method"))
+setGeneric("sDev", function(object, ...) standardGeneric("sDev"))
+
 
 ##setGeneric("biplot", function(x, ...) standardGeneric("biplot"))
 ##setGeneric("biplot", function(x, choices=1:2, scale=1, pc.biplot=FALSE, ...) standardGeneric("biplot"))
